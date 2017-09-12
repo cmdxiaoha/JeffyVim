75a896324db3c547d6f5a4fd4c645917768cc913
cmdxiaoha <cmdxiaoha@163.com>
2017-09-12 15:24:37 +0800

    add global .ycm_extra_conf.py

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+
 cp ./_vimrc ~/.vimrc
+cp ./_ycm_extra_conf.py ~/.ycm_extra_conf.py
+
 vim +PluginInstall +qall
+
